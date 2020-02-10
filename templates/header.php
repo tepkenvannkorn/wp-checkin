32a1ec162b21ce5321274982cca8e4d508ce4098
Naoko Takano <naokomc@gmail.com>
2020-02-11 01:33:03 +0900

    Translate templates
    Replaced Japanese strings to English

--- templates/header.php
+++ templates/header.php
@@ -1,10 +1,10 @@
 <!doctype html>
-<html lang="ja" prefix="og: http://ogp.me/ns#">
+<html lang="en" prefix="og: http://ogp.me/ns#">
 <head>
     <meta charset="utf-8"/>
-    <title>WP Checkin - Tokyo 2019</title>
+    <title>WP Checkin - Asia 2020</title>
     <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
-    <meta name="description" content="A Reception Tool for WordCamp Tokyo 2019" />
+    <meta name="description" content="A registration tool for WordCamp Asia 2020" />
 
 	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css" integrity="sha384-GJzZqFGwb1QTTN6wy59ffF1BuGJpLSa9DkKMp0DgiMDm4iYMj70gZWKYbI706tWS" crossorigin="anonymous">
     <link href='/assets/css/style.css?s=<?= filemtime(dirname(__DIR__) . '/public/assets/css/style.css') ?>'
@@ -18,7 +18,7 @@
 <nav class="navbar navbar-dark bg-success">
 	<a class="navbar-brand" href="#">
 		<img src="/assets/img/wapuu.png" width="30" height="30" alt="Wapuu">
-		WordCamp Tokyo 2019 Reception
+		WordCamp Asia 2020 Registration
 	</a>
 </nav>
 	<div class="container container-main">
